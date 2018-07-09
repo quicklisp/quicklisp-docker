705299eb984a13a9ac04a527a7b7f60b6a88d44b
xach <xach@hornbeam.talmadge.xach.com>
2018-07-08 20:58:43 -0400

    Fix up building and running of debian 8.

--- debian8/run.sh
+++ debian8/run.sh
@@ -1,6 +1,7 @@
-IMAGE=${1:-qld8}
+IMAGE=${1:-quicklisp/quicklisp-base:debian8}
+NAME=qlb
 
-docker run --rm -it --name $IMAGE \
+docker run --rm -it --name $NAME \
        -v $HOME/build/upstream-cache:/home/quicklisp/quicklisp-controller/upstream-cache \
        -v $HOME/build/dist:/home/quicklisp/quicklisp-controller/dist \
        -v $HOME/build/quicklisp-projects:/home/quicklisp/quicklisp-projects \
